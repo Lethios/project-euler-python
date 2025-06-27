8f3c134822ed6824f69d575f07ac18f672e04fc5
Lethios <204111230+Lethios@users.noreply.github.com>
2025-06-27 14:23:10 +0530

    Update 053.py

--- solutions/053.py
+++ solutions/053.py
@@ -1,1 +1,11 @@
+from math import comb
 
+ans_count = 0
+
+for n in range(1, 101):
+    for r in range(1, n):
+        if comb(n, r) > 1_000_000:
+            ans_count += 1
+
+print(ans_count)
+# 4075

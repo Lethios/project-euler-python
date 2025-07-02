27648106526f7405565ac87c35b91d66ee48b602
Lethios <204111230+Lethios@users.noreply.github.com>
2025-07-02 15:41:35 +0530

    Update 0003.py

--- solutions/0003.py
+++ solutions/0003.py
@@ -1,10 +1,4 @@
-"""
-https://projecteuler.net/problem=3
-
-The prime factors of 13195 are 5, 7, 13, and 29.
-
-What is the largest prime factor of the number 600851475143?
-"""
+# https://projecteuler.net/problem=3
 
 def prime_factor(n):
     i = 2

eb60eba6783a35ce1f251bd85837b931e4b38a28
Lethios <204111230+Lethios@users.noreply.github.com>
2025-07-03 10:06:06 +0530

    Update 0005.py

--- solutions/0005.py
+++ solutions/0005.py
@@ -1,10 +1,4 @@
-"""
-https://projecteuler.net/problem=5
-
-2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-
-Find the smallest number that is evenly divisible by all of the numbers from 1 to 20.
-"""
+# https://projecteuler.net/problem=5
 
 from math import lcm
 from functools import reduce

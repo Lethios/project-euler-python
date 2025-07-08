b69340a7b9b9ee3af149bafbedf9f7b4ff91d4c8
Lethios <204111230+Lethios@users.noreply.github.com>
2025-07-08 10:44:49 +0530

    Update 0016.py

--- solutions/0016.py
+++ solutions/0016.py
@@ -1,10 +1,4 @@
-"""
-https://projecteuler.net/problem=16
-
-2¹⁵ = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-
-What is the sum of the digits of the number 2¹⁰⁰⁰?
-"""
+# https://projecteuler.net/problem=16
 
 def power_digit_sum(base, power):
     power_num = str(int(pow(base, power)))

56653703046a61c59db6393b384796b3cc307010
Lethios <204111230+Lethios@users.noreply.github.com>
2025-07-10 11:19:07 +0530

    Update 0048.py

--- solutions/0048.py
+++ solutions/0048.py
@@ -1,11 +1,6 @@
 """
 https://projecteuler.net/problem=48
 
-The series, 1¹ + 2² + 3³ + ··· + 10¹⁰ = 10405071317.
-
-Find the last ten digits of the series, 1¹ + 2² + 3³ + ··· + 1000¹⁰⁰⁰.
-"""
-
 def power_sum_series(n):
     number_sum = 0
 

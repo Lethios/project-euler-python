6ce8a9814e92589d3401e216b33a7597d34d4941
Lethios <204111230+Lethios@users.noreply.github.com>
2025-07-15 10:22:53 +0530

    Update 0063.py

--- solutions/0063.py
+++ solutions/0063.py
@@ -1,11 +1,6 @@
 """
 https://projecteuler.net/problem=63
 
-The 5-digit number, 16807 = 7⁵, is also a fifth power. Similarly, the 9-digit number, 134217728 = 8⁹, is a ninth power.
-
-How many n-digit positive integers exist which are also an nth power?
-"""
-
 count = 0
 
 for power in range(1, 5000):
